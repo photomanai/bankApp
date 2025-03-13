a6236037b1cfa68b51fb211d2de8c4bfcbeea0de
photomanai <photom4n.41@gmail.com>
2025-03-13 22:53:36 +0400

    verify middleware

--- api/routers/cardRouter.js
+++ api/routers/cardRouter.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/cardConstroller");
-const { verify } = require("../middleware/authMiddleware");
 
 router.post("/create", controller.create);
 router.post("/call", controller.callCards);
